bbc3f34c128919147eb2195178e361e08a94d3b8
grakp <grace.zhao325@gmail.com>
2024-10-16 11:16:54 -0400

    player movement + animations working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+    private PlayerInputActions inputActions;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Move.performed += Move;
+        inputActions.Player.Move.canceled += Move;
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= Move;
+        inputActions.Player.Move.canceled -= Move;
+        inputActions.Disable();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool("isWalking", moveInput.sqrMagnitude > 0);
+        animator.SetFloat("LastInputX", moveInput.x);
+        animator.SetFloat("LastInputY", moveInput.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = moveInput.normalized * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("moving");
+
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled) // if stopped moving
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
+        moveInput = context.ReadValue<Vector2>();
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
+
+    
+}
