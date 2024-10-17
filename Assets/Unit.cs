56b9f085655802066e7ac9d3affd57c9ec60c786
grakp <grace.zhao325@gmail.com>
2024-10-16 21:21:34 -0400

    battle system wip

--- Assets/Unit.cs
+++ Assets/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,4 +9,17 @@ public class Unit : MonoBehaviour
     public int damage;
     public int maxHP;
     public int currentHP;
+
+    public bool TakeDamage(int dmg)
+    {
+        currentHP -= dmg;
+
+        if (currentHP <= 0)
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
